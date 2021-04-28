626ba274601e63ac7164d3dcf2bf2b04ffb06c6b
zhangwq <zhangwq@zhangwq.me>
2021-04-28 19:06:39 +0800

    week01 homework#2

---
+++ code/src/main/java/demo/jvm0104/MovingAverage.java
@@ -0,0 +1,14 @@
+package demo.jvm0104;
+// 移动平均数
+public class MovingAverage {
+    private int count = 0;
+    private double sum = 0.0D;
+    public void submit(double value) {
+        this.count++;
+        this.sum += value;
+    }
+    public double getAvg() {
+        if (0 == this.count) {return this.sum;}
+        return this.sum / this.count;
+    }
+}
---
+++ code/src/main/java/homework/week01/MyClassLoader.java
@@ -0,0 +1,60 @@
+package homework.week01;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+/*
+自定义一个 Classloader
+加载一个 Hello.xlass 文件 执行 hello 方法
+此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件
+ */
+public class MyClassLoader extends ClassLoader {
+    // 读取反码类字节
+    byte[] getHelloXclass() throws IOException {
+        // Hello.class 文件所有字节（x=255-x）处理后的文件
+        InputStream inputStream = MyClassLoader.class.getClassLoader().getResourceAsStream("Hello.xlass");
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int len = -1;
+        byte[] buff = new byte[1024];
+        while ((len = inputStream.read(buff)) != -1) {
+            outputStream.write(buff, 0, len);
+        }
+        outputStream.close();
+        inputStream.close();
+        return outputStream.toByteArray();
+    }
+    // 获取原始类字节
+    private static byte[] getHelloClassBytes(byte[] xclass) {
+        byte[] xclass2 = new byte[xclass.length];
+        int i = 0, len = xclass.length;
+        for (; i < len; i++) {
+            // b = 255 - b0
+            byte b = xclass[i];
+            // 反码
+            // (b & 0xff);
+            byte b0 = (byte) ~b;
+            xclass2[i] = b0;
+        }
+        return xclass2;
+    }
+    // 类直接保存到文件
+    // 通过 javap 手动查看类文件
+    static void writeByte2File(String path, byte[] xclass2) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        fileOutputStream.write(xclass2);
+        fileOutputStream.close();
+    }
+    public static void main(String[] args) throws IOException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        MyClassLoader myClassLoader = new MyClassLoader();
+        byte[] xclass = myClassLoader.getHelloXclass();
+        byte[] xclass2 = getHelloClassBytes(xclass);
+        Class helloClass = myClassLoader.defineClass("Hello", xclass2, 0, xclass2.length);
+        Object hello = helloClass.newInstance();
+        Method helloMethod = helloClass.getMethod("hello");
+        // Hello, classLoader!
+        helloMethod.invoke(hello);
+    }
+}
