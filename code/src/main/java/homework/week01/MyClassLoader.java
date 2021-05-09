263f614ac3d583d2f38ccbe84cd4d8f5ebad90f0
zhangwq <zhangwq@zhangwq.me>
2021-05-09 22:07:29 +0800

    第一周作业#2

--- code/src/main/java/homework/week01/MyClassLoader.java
+++ code/src/main/java/homework/week01/MyClassLoader.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 /*
+第一周作业#2
 自定义一个 Classloader
 加载一个 Hello.xlass 文件 执行 hello 方法
 此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件
